4e77f2dbd44c42e107d9c022e1d596ed305db1ba
Yuanchun Shen <y.c.shen@tum.de>
2023-03-04 17:17:28 +0100

    WIP: adapt flamingo base model for t5 decoder

--- models/flamingo_t5.py
+++ models/flamingo_t5.py
@@ -4,7 +4,8 @@ This file patches the flamingo gated cross attention into the T5 decoder.
 import contextlib
 import logging
 from abc import ABC, abstractmethod
-from typing import Dict, List
+from typing import Dict, List, Optional
+import dataclasses
 from dataclasses import dataclass
 
 import torch
@@ -13,14 +14,13 @@ from einops import rearrange
 from torch import nn
 from transformers import PreTrainedModel
 from transformers.modeling_outputs import (BaseModelOutputWithPast,
-                                           CausalLMOutputWithPast)
+                                           CausalLMOutputWithPast,
+                                           CausalLMOutputWithCrossAttentions)
 from transformers.models.t5.modeling_t5 import (T5Block, T5LayerCrossAttention,
                                                 T5LayerFF,
                                                 T5LayerSelfAttention)
 
 from .gated_xattn import HijackedLMBlock
-
-logger = logging.getLogger(__name__)
 
 
 @contextlib.contextmanager
@@ -38,19 +38,38 @@ def suppress_model_loading_warnings(suppress: bool = True):
 @dataclass
 class FlamingoConfig:
     """Configuration class to store the configuration of a `FlamingoT5` model."""
-    xattn_every: int = 1
+    dim: int                # dimension of the token embedding
+    dim_media: int          # dimension of the media embedding
+    xattn_dim_head: int     # dimension of the cross attention heads
+    xattn_every: int = 1    # how often to insert a gated cross attention layer
+    xattn_ff_mult: int = 4  # multiplier for the feedforward layer in the gated cross attention
 
 
-class FlamingoBaseModel(ABC, PreTrainedModel):
-    """ 
-    abstract class, which is inherited by FlamingoGPT2 and FlamingoOPT.
+@dataclass
+class FlamingoCausalLMOutputWithCrossAttentions(CausalLMOutputWithCrossAttentions):
+    """Add cross attentions past key values to the base class."""
+    xattn_past_key_values: tuple[torch.FloatTensor] = None
+
+
+class FlamingoDecoderBaseModel(ABC, PreTrainedModel):
+    """
+    abstract class, which is inherited by FlamingoT5.
+    Different from dhansmair's implementation, this class assumes the lm model is a decoder
+    with a lm head. The labels are processed and the loss is calculated in lm's forward() pass.
     This class provides the core functionalities of Flamingo: the forward() function,
     setting up the resampler and hijacking the LM layers with GatedXAttn layers.
+
+    Modification upon dhansmair's implementation:
+    - removed vision encoder
+    - removed resampler
+    - freeze token embedding of the language model
+    - remove lm head
+    - removed pixels
+    - media and media_mask are nullable. If so, the cross attention on them will be skipped.
     """
 
     config: FlamingoConfig
     lm: PreTrainedModel
-    lm_head: nn.Linear
 
     config_class = FlamingoConfig
 
@@ -64,46 +83,36 @@ class FlamingoBaseModel(ABC, PreTrainedModel):
         careful, this method will modify the LM layers!
         """
         for i, lm_layer in enumerate(lm_layers):
-            if i % self.config.xattn_every != 0: 
+            if i % self.config.xattn_every != 0:
                 continue
 
             lm_layers[i] = HijackedLMBlock(
                 lm_layer,
                 dim=self.config.dim,
-                dim_visual=self.config.dim_visual,
+                dim_media=self.config.dim_media,
                 dim_head=self.config.xattn_dim_head,
                 heads=self.config.xattn_heads,
                 ff_mult=self.config.xattn_ff_mult,
-                act=self.config.xattn_act,
-                n_visual=self.config.resampler_num_latents
             )
 
     @abstractmethod
     def get_modified_layers(self) -> List[HijackedLMBlock]:
         raise NotImplementedError
 
-    def freeze_vm(self):
-        """freeze vision model """
-        for param in self.vision_encoder.parameters():
-            param.requires_grad = False
-
     def freeze_lm(self):
         """ freeze weights of the language model.
-
-        (!) does not freeze token embedding matrix and gated xattn layers
         """
 
         for param in self.lm.parameters():
             param.requires_grad = False
-
-        # lm_head shares weights with the embeddings so no need to unfreeze that as well
-        self.lm.get_input_embeddings().weight.requires_grad = True
 
         for xattn in self.get_modified_layers():
             for param in xattn.xattn_block.parameters():
                 param.requires_grad = True
 
     def unfreeze_lm(self):
+        """Unfreeze all weights of the language model.
+        """
         for param in self.lm.parameters():
             param.requires_grad = True
 
@@ -118,150 +127,87 @@ class FlamingoBaseModel(ABC, PreTrainedModel):
         """Access the trainable parameters, e.g. useful for the optimizer and gradient clipping. 
 
         example: optimizer = AdamW(model.parameters_trainable(), lr=args.lr)
-        make sure to call freeze_lm() first! 
+        make sure to call freeze_lm() first!
         """
         return filter(lambda p: p.requires_grad, self.parameters())
-    
-    def encode_resample_visuals(self, pixel_values: torch.Tensor) -> torch.Tensor:
-        """pass pixel values through vision encoder and perceiver resampler.
 
-        Args:
-            pixel_values (torch.Tensor): accepted shapes:
-                (N c h w)       one batch, multiple images
-                (b N c h w)     multiple batches, multiple images
-                (b N T c h w)   multiple batches, multiple images, multiple frames
-
-        Returns:
-            (torch.Tensor): shape (b N q d)
-        """
-
-        if pixel_values.ndim == 4:            
-            # (N c h w)
-            b, N, T = 1, pixel_values.size(0), 1
-        
-        elif pixel_values.ndim == 5:       
-            # (b N c h w)
-            b, N, T = *pixel_values.shape[:2], 1
-            pixel_values = rearrange(pixel_values, 'b N c h w -> (b N) c h w')
-
-        elif pixel_values.ndim == 6:         
-            # (b N T c h w) -> (b N T v d)
-            b, N, T = pixel_values.shape[:3]
-            pixel_values = rearrange(pixel_values, 'b N T c h w -> (b N T) c h w')
-        else:
-            raise ValueError('pixel_values must have ndim 5 or 6!')
-
-        with torch.no_grad():
-            visual_features = self.vision_encoder(pixel_values).last_hidden_state         # (b N T) v d
-
-        # perceiver resampler
-        # (only need to do if kv of the xattn layers were not calculated yet.)
-        # resample visual features ((b N T) v d) -> (b N T q d)
-        visual_features = rearrange(visual_features, '(b N T) v d -> (b N) T v d', b=b, N=N, T=T)
-        visual_features = self.resampler(visual_features)
-
-        # T is gone at this point
-        visual_features = rearrange(visual_features, '(b N) q d -> b N q d', b=b, N=N)
-        
-        return visual_features
-        
     def forward(
         self,
-        input_ids: torch.Tensor | None = None,
-        attention_mask: torch.Tensor | None = None,
-        media_locations: torch.Tensor | None = None,
-        pixel_values: torch.Tensor | None = None,
-        visual_features: torch.Tensor | None = None,
-        head_mask: torch.Tensor | None = None,
-        inputs_embeds: torch.Tensor | None = None,
+        input_ids: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+        media: Optional[torch.Tensor] = None,
+        media_mask: Optional[torch.Tensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        past_key_values: Optional[tuple] = None,
+        xattn_past_key_values: Optional[tuple] = None,
+        labels: Optional[torch.Tensor] = None,
         use_cache: bool = False,
-        past_key_values: tuple | None = None,
         return_dict: bool = True,
-        labels: torch.Tensor | None = None,
-        loss_reduction: str = 'mean',
         **kwargs
-    ) -> CausalLMOutputWithPast:
+    ) -> FlamingoCausalLMOutputWithCrossAttentions:
         """Flamingo forward pass
 
-        Most of the parameters are inspired by huggingface language model implementations, so this doc may be informative:
-        https://huggingface.co/docs/transformers/model_doc/gpt2#transformers.GPT2Model.forward
+        The forward pass is closely coupled to the T5ForCaualLM's forward pass (implemented within this repo).
 
         Args:
             input_ids (Tensor | None):         shape (n_batch, n_tokens). the tokenized input text
             attention_mask (Tensor | None):    shape (n_batch, n_tokens). 
                 Mask as produced by the tokenizer. Required when a batch of input strings are tokenized and thus padded at the end.
                 Then this will indicate the locations of 'real' tokens vs. the location of 'pad' tokens.
-            media_locations (Tensor | None):   shape (n_batch, n_tokens).
-                indicates the locations of the starts of the <image> tags beginning, i.e. the location of the token representing '<'
-            pixel_values (Tensor | None):    shape (b N T c h w). Optional.
-            visual_features (Tensor | None):         shape (b N q d). Optional.
+            inputs_embeds (Tensor | None):   shape (n_batch, seq_len, hidden_size)
+                Optionally, instead of passing `input_ids` you can choose to directly pass an embedded representation. This
+                is useful if you want more control over how to convert `input_ids` indices into associated vectors than the
+                model's internal embedding lookup matrix.
+            media (Tensor | None):         shape (b N q d). Optional.
                 If pixel_values already have been passed through encode_resample_visuals(), 
                 you can pass the resampled visual embeddings via this parameter.
                 If provided, pixel_values will be ignored
-            head_mask (Tensor | None): TODO
-            inputs_embeds (Tensor | None): TODO
-            use_cache (bool): whether to return the inner keys and values. Used to speed up text generation at inference. defaults to False
-            past_key_values (tuple): tuple of past_key_values of (1) the xattn layers (2) the language model
-            return_dict (bool): Whether to return a dictionary. Right now, only dicts are supported, so this must be set to True. Defaults to True.
+            media_mask (Tensor | None):   shape (n_batch, n_latents).
+                Mask for the media tokens. If None, all media tokens will be regarded valid.
+            head_mask (Tensor | None):  shape (n_heads,) or (num_layers, n_heads)
+                Mask to nullify selected heads of the self-attention modules in the encoder.
+            past_key_values (tuple):
+                past_key_values of the language model
+            xattn_past_key_values (tuple):
+                past_key_values of the cross attention on media
             labels (Tensor): 
                 It is possible to pass the exact value as input_ids also as labels. If present, the output will contain a CE loss of the next token prediction.
                 optional, defaults to None
+            use_cache (bool): whether to return the inner keys and values. Used to speed up text generation at inference. defaults to False
+            return_dict (bool): Whether to return a dictionary. Right now, only dicts are supported, so this must be set to True. Defaults to True.
             **kwargs
 
         Returns:
-            (CausalLMOutputWithPast): an object containing all the useful stuff. Refer to hf documentation.
-
+            (FlamingoCausalLMOutputWithCrossAttentions): an object containing all the useful stuff. Refer to hf documentation.
         """
 
         # sanity check
         assert return_dict, "can only use return_dict=True at the moment!"
         assert (input_ids is None) != (inputs_embeds is None), "you must pass either input_ids or inputs_embeds!"
 
-        # find the input shape
-        batch_size, seq_length = input_ids.shape[:2] if input_ids is not None else inputs_embeds.shape[:2]
-        device = input_ids.device if input_ids is not None else inputs_embeds.device
-        xattn_past_key_values = None if past_key_values is None else past_key_values[0]
-        lm_past_key_values = None if past_key_values is None else past_key_values[1]
-        
-        if visual_features is None:
-            if xattn_past_key_values is None and pixel_values is not None:
-                # extract from pixels
-                assert pixel_values.size(0) == batch_size, \
-                    "pixel_values must have the same batch size as the textual input!"
-                
-                visual_features = self.encode_resample_visuals(pixel_values)
-                
-            else:
-                # we don't need visual_features is past is defined.
-                # use dummy values, since are only required for the shape
-                # visual_embedings shape (b N q d)
-                visual_features = torch.zeros(
-                    (batch_size, 1, self.config.resampler_num_latents, self.config.dim_visual),
-                    dtype=torch.float32,
-                    device=device
-                )
-
-        if media_locations is None:
-            media_locations = torch.zeros(size=(batch_size, seq_length), dtype=torch.int, device=device)
+        if media_mask is None and media is not None:
+            # media is of shape (batch_size, n_latents, d_media)
+            batch_size, n_latents = media.shape[:2]
+            media_mask = torch.ones(size=(batch_size, n_latents), dtype=torch.int, device=media_mask.device)
 
         # condition xattn layers
         for i, xattn in enumerate(self.get_modified_layers()):
             layer_past = None if xattn_past_key_values is None else xattn_past_key_values[i]
-            xattn.condition(visual_features, media_locations, layer_past)
+            xattn.condition(media, media_mask, layer_past)
 
         # pass through LM
-        out: BaseModelOutputWithPast = self.lm(
+        out: CausalLMOutputWithCrossAttentions = self.lm(
             input_ids=input_ids,
             attention_mask=attention_mask,
-            head_mask=head_mask,
             inputs_embeds=inputs_embeds,
+            head_mask=head_mask,
+            labels=labels,
             use_cache=use_cache,
-            past_key_values=lm_past_key_values,
+            past_key_values=past_key_values,
             return_dict=True,
             **kwargs
         )
-
-        logits: torch.Tensor = self.lm_head(out.last_hidden_state)
 
         # collect the past_key_values from the xattn layers
         if use_cache:
@@ -269,23 +215,7 @@ class FlamingoBaseModel(ABC, PreTrainedModel):
             for modified_layer in self.get_modified_layers():
                 xattn_past_key_values.append(modified_layer.kv_output)
 
-        loss = None
-        if labels is not None:
-            # loss function calculation, inspired by hf implementations
-            # Shift so that tokens < n predict n
-            # logits shape (batch, seq_length, #words)
-            shift_logits = logits[..., :-1, :].contiguous()
-            # labels shape (batch, seq_length)
-            shift_labels = labels[..., 1:].contiguous()
+        out = FlamingoCausalLMOutputWithCrossAttentions(**dataclasses.asdict(out))
+        out.xattn_past_key_values = tuple(xattn_past_key_values) if use_cache else None
 
-            # Flatten the tokens
-            loss = F.cross_entropy(shift_logits.view(-1, shift_logits.size(-1)),
-                                   shift_labels.view(-1), reduction=loss_reduction)
-
-        return CausalLMOutputWithPast(
-            loss=loss,
-            logits=logits,
-            past_key_values=(tuple(xattn_past_key_values), out.past_key_values) if use_cache else None,
-            hidden_states=out.hidden_states,
-            attentions=out.attentions,
-        )
+        return out
