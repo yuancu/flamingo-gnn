1ae2def0b60f163d4ae3489b201fc01300cfbeb8
Yuanchun Shen <y.c.shen@tum.de>
2023-03-27 19:05:56 +0200

    Add tddp test

---
+++ tests/test_ddp.py
@@ -0,0 +1,26 @@
+"""Test DDP training with WandbLogger."""
+import pytest
+import torch
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import WandbLogger
+
+from lightning.lit_seq2seq import LitT5Seq2Seq
+
+
+
+def test_ddp_and_wandb(args, train_loader, encoder, decoder):
+    if torch.cuda.device_count() == 0:
+        # skip test if no GPU
+        pytest.skip('No GPU available for testing DDP')
+    model = LitT5Seq2Seq(
+        args=args,
+        encoder=encoder,
+        decoder=decoder,
+        freeze_lm=False,
+        freeze_non_lm=False,
+        do_validation=False)
+    wandb_logger = WandbLogger(project=args.wandb_project, offline=True)
+    device_cnt = 2 if torch.cuda.device_count() > 1 else 1
+    trainer = Trainer(accelerator='gpu', strategy='ddp', devices=device_cnt,
+                      logger=wandb_logger, fast_dev_run=True)
+    trainer.fit(model, train_loader)
