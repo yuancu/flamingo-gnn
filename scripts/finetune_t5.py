a269efd515ec91baf0259b846004aae8dffea10b
Yuanchun Shen <y.c.shen@tum.de>
2023-05-11 17:08:20 +0200

    Add a finetune scripts on T5 model alone

---
+++ scripts/finetune_t5.py
@@ -0,0 +1,122 @@
+"""Pretrain the T5-based encoder-decoder architectured dragon model.
+"""
+import os
+from argparse import ArgumentParser
+from datetime import datetime
+from pathlib import Path
+
+import pytorch_lightning as pl
+import torch
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
+from transformers import AutoModelForSeq2SeqLM
+
+from dataset.lmgnn import load_data
+from lightning.lit_t5 import LitT5
+from utils.common import load_args
+
+def freeze_params(model, num_trainable_blocks):
+    """Freeze all the parameters except the last num_trainable_blocks 
+    and lm head in the decoder and the lm_head.
+
+    Args:
+        model (T5ForConditionalGeneration)
+        num_trainable_blocks (int): Number of trainable blocks in the decoder.
+    """
+    for param in model.parameters():
+        param.requires_grad = False
+    num_decoder_blocks = len(model.decoder.block)
+    for i, block in enumerate(model.decoder.block):
+        if i > num_decoder_blocks - num_trainable_blocks - 1:
+            for param in block.parameters():
+                param.requires_grad = True
+    for param in model.lm_head.parameters():
+        param.requires_grad = True
+
+
+def main(args):
+    # 0. Set seed
+    # Setting all the random seeds to the same value.
+    # This is important in a distributed training setting. 
+    # Each rank will get its own set of initial weights. 
+    # If they don't match up, the gradients will not match either,
+    # leading to training that may not converge.
+    pl.seed_everything(1)
+
+    # 1. Load configs
+    run_name = args.run_name
+    num_trainable_blocks = args.num_trainable_blocks
+    mode = 'finetune'
+    config_profile = args.config_profile
+    args = load_args(config_path=args.config, profile=args.config_profile)
+    args.run_name = run_name
+
+    # 2. Load data
+    # Set collator and dataset according to the task: pretrain is mainly devided into two types:
+    # with or without graph
+    dummy_graph = True
+    train_kwargs={'encoder_input': args.encoder_input, 'decoder_label': args.decoder_label}
+    if mode == 'pretrain':
+        val_decoder_label = args.decoder_label
+    else:
+        val_decoder_label = 'raw_answers'
+    val_kwargs={'encoder_input': args.encoder_input, 'decoder_label': val_decoder_label}
+    train_loader, val_loader = load_data(
+        args,
+        corrupt=False,
+        dummy_graph=dummy_graph,
+        num_workers=8,
+        train_kwargs=train_kwargs,
+        val_kwargs=val_kwargs,)
+
+    # 3. Create encoder and decoder
+    t5 = AutoModelForSeq2SeqLM.from_pretrained(args.encoder_name_or_path)
+    freeze_params(t5, num_trainable_blocks)
+
+    # 4. Create pytorch lightning model
+    model = LitT5(args, t5)
+
+    # 5. Create trainer
+    now = datetime.now().strftime('%d%H%M')
+    run_name = f"{args.run_name}-{now}"
+    offline = args.wandb_mode in ['offline', 'disabled']
+    Path(args.log_dir).mkdir(parents=True, exist_ok=True)
+    wandb_logger = WandbLogger(project=args.wandb_project, offline=offline, name=run_name,
+                               group=config_profile, save_dir=args.log_dir)
+    wandb_logger.experiment.config.update(vars(args))
+    if mode == 'finetune':
+        checkpoint_callback = ModelCheckpoint(monitor="em", mode="max", save_weights_only=True,)
+        callbacks = [checkpoint_callback]
+    else:
+        callbacks = None
+    trainer = pl.Trainer(max_epochs=args.n_epochs, fast_dev_run=args.fast_dev_run,
+                         default_root_dir=os.path.join(args.save_dir, args.run_name),
+                         accelerator='gpu', strategy=args.strategy, logger=wandb_logger,
+                         callbacks=callbacks, gradient_clip_val=0.5,
+                         accumulate_grad_batches=8)
+
+    # 6. Train
+    if args.restore_training:
+        resume_checkpoint = args.checkpoint_path
+        assert resume_checkpoint, "No checkpoint to resume training. (got {resume_checkpoint})"
+    else:
+        resume_checkpoint = None
+    trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader,
+                ckpt_path=resume_checkpoint)
+
+
+if __name__ == '__main__':
+    # To properly utilize a CUDA device with tensor cores
+    torch.set_float32_matmul_precision('medium')
+    torch.backends.cudnn.benchmark = True
+    # To avoid 'too many open files' error
+    torch.multiprocessing.set_sharing_strategy('file_system')
+
+    # Parse arguments
+    parser = ArgumentParser()
+    parser.add_argument('--config', type=str, default='configs/lmgnn.yaml')
+    parser.add_argument('--config-profile', type=str, required=True)
+    parser.add_argument('--run-name', type=str, required=True)
+    parser.add_argument('--num-trainable-blocks', type=int, help='Number of trainable blocks in the decoder.')
+    args = parser.parse_args()
+    main(args)
