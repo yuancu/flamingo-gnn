34973661c5e8095f2ef18a99dc13a7a2026a169d
Yuanchun Shen <y.c.shen@tum.de>
2023-05-18 09:49:13 +0200

    Add sanity check for csqa

---
+++ scripts/ft-csqa-sanity.sh
@@ -0,0 +1,5 @@
+export TOKENIZERS_PARALLELISM=true
+export WANDB__SERVICE_WAIT=300
+
+# run pretrain
+python -u train.py --finetune --multiple-choice --config configs/csqa.yaml --config-profile finetune_csqa_sanity --run-name ftcsqa-sanity
