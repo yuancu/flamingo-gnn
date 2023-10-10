45dbae3b78ac3b4d157f8bf236ae9e86a00d5c8d
Yuanchun Shen <y.c.shen@tum.de>
2023-10-10 14:52:58 +0200

    Update training configs for gqa

---
+++ scripts/gqa-c-ret.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Finetune on Multilingual Complex Wikidata Questions
+#
+#SBATCH --job-name gqa-cret
+#SBATCH --output=runs/R-%x.%j.out
+#SBATCH --ntasks=1
+#SBATCH --time=48:00:00
+#SBATCH --gres=gpu:1
+
+# debug info
+hostname
+which python
+nvidia-smi
+
+# venv
+source ~/miniconda3/bin/activate dragon
+
+# test cuda
+python -c "import torch; print('device_count:', torch.cuda.device_count())"
+python -c "import torch_geometric; print('torch_geometric version:', torch_geometric.__version__)"
+
+export TOKENIZERS_PARALLELISM=true
+export WANDB__SERVICE_WAIT=300
+
+# Experiment c: CrossGNN with Graphs (No warmup; no frozen)
+python -u train.py --finetune --config configs/gqa.yaml --config-profile finetune_gqa_xwxf_ret \
+    --run-name gqa-c-xwxf-ret --tune-lr
---
+++ scripts/gqa-e-ret.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Finetune on Multilingual Complex Wikidata Questions
+#
+#SBATCH --job-name gqa-eret
+#SBATCH --output=runs/R-%x.%j.out
+#SBATCH --ntasks=1
+#SBATCH --time=48:00:00
+#SBATCH --gres=gpu:1
+
+# debug info
+hostname
+which python
+nvidia-smi
+
+# venv
+source ~/miniconda3/bin/activate dragon
+
+# test cuda
+python -c "import torch; print('device_count:', torch.cuda.device_count())"
+python -c "import torch_geometric; print('torch_geometric version:', torch_geometric.__version__)"
+
+export TOKENIZERS_PARALLELISM=true
+export WANDB__SERVICE_WAIT=300
+
+
+# Experiment e: CrossGNN with FrozenLM (No warmup, freeze LM)
+python -u train.py --finetune --config configs/gqa.yaml --config-profile finetune_gqa_xw_ret \
+    --run-name gqa-e-xw-ret --tune-lr
